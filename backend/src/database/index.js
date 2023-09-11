2df1ab3307c3c509e13c8e143888f118fe31f9aa
Sachin <sachinsunny303@gmail.com>
2023-09-11 19:47:18 +0100

    backend complete

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.UserName}:${process.env.Password}@${process.env.MongoDB_URI}/UsernameDB`
-  )
+const {connect, Schema, model} = mongoose;
+
+connect(
+  `mongodb+srv://${process.env.UserName}:${process.env.Password}@${process.env.MongoDB_URI}/UsernameDB`
+)
   .then(() => console.log('connected to DB'))
   .catch((err) => console.log(err));
+
+const userSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: String,
+  email: String,
+});
+
+const UserModel = model('User', userSchema);
+
+module.exports = UserModel;
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,38 +1,64 @@
 const {Router} = require('express');
+const UserModel = require('../database/index');
 const router = Router();
 
-const userDB = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    email: 'jane.smith@example.com',
-  },
-  {
-    id: 3,
-    name: 'Bob Johnson',
-    email: 'bob.johnson@example.com',
-  },
-  {
-    id: 4,
-    name: 'Alice Brown',
-    email: 'alice.brown@example.com',
-  },
-  {
-    id: 5,
-    name: 'Charlie Wilson',
-    email: 'charlie.wilson@example.com',
-  },
-];
+router.get('/', async (request, response) => {
+  try {
+    const data = await UserModel.find();
+    response.send(data);
+  } catch (err) {
+    response.sendStatus(400).send(err);
+  }
+});
 
-router.get('/:id', (request, response) => {
+router.post('/', async (request, response) => {
+  const {body} = request;
+  try {
+    const data = await UserModel.create(body);
+    response.send(data);
+  } catch (error) {
+    response.sendStatus(400).send(error);
+  }
+});
+
+router.get('/:id', async (request, response) => {
   const {id} = request.params;
 
-  response.send(userDB);
+  try {
+    const data = await UserModel.find({id: id});
+    data.length > 0 ? response.send(data) : response.sendStatus(404);
+  } catch (error) {
+    response.sendStatus(400).send(error);
+  }
+});
+
+router.delete('/:id', async (request, response) => {
+  const {id} = request.params;
+
+  try {
+    const deletedJson = await UserModel.findOneAndDelete({id: id});
+    response.send(deletedJson);
+  } catch (error) {
+    response.sendStatus(400).send(error);
+  }
+});
+
+router.put('/:id', async (request, response) => {
+  const {id} = request.params;
+  const {body} = request;
+
+  try {
+    const updatedJson = await UserModel.findOneAndUpdate(
+      {id: id},
+      body,
+      {
+        new: true,
+      }
+    );
+    response.send(updatedJson);
+  } catch (error) {
+    response.sendStatus(400).send(error);
+  }
 });
 
 module.exports = router;
