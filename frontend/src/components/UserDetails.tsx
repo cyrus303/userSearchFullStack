e5e37830d8387d5386fb211eba2e18246f1ad5a0
Sachin <sachinsunny303@gmail.com>
2023-09-14 20:23:06 +0100

    assignment completed

--- frontend/src/components/UserDetails.tsx
+++ frontend/src/components/UserDetails.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ApiResponse} from './SearchComponent';
 import axios from 'axios';
 
@@ -10,15 +10,37 @@ const UserDetails: React.FC<UserDetailsProps> = (props) => {
   const {userInfo} = props;
   const {email, name, id} = userInfo[0];
   const [deleteStatus, setDeleteStatus] = useState(false);
+  const [edit, setEdit] = useState(true);
+
+  const [inputName, setInputName] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
+
+  useEffect(() => {
+    setInputEmail(email);
+    setInputName(name);
+    setDeleteStatus(false);
+  }, [email, name]);
 
   const handleClick = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     deleteUser();
     setDeleteStatus(true);
-    setTimeout(() => {
-      setDeleteStatus(false);
-    }, 1000);
+  };
+
+  const handleEdit = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setEdit(false);
+  };
+
+  const handleSave = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setEdit(true);
+    console.log('name:', name);
+    console.log('inputname:', inputName);
+    updateUser();
   };
 
   const deleteUser = async () => {
@@ -32,37 +54,90 @@ const UserDetails: React.FC<UserDetailsProps> = (props) => {
     }
   };
 
+  const updateUser = async () => {
+    try {
+      const response = await axios({
+        method: 'put',
+        url: 'http://localhost:3001/api/user/' + id,
+        data: {
+          id: 1,
+          name: inputName,
+          email: inputEmail,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="user-container">
       {deleteStatus === false ? (
         <div className="card">
-          <button className="dismiss" type="button">
+          {/* <button className="dismiss" type="button">
             ×
-          </button>
+          </button> */}
           <div className="header">
             <div className="content">
               <span className="title">User Details</span>
               <div className="message">
                 <div className="text-container">
-                  <span className="span-label">ID:</span> {id}
+                  <span className="span-label">ID:</span>
+                  {id}
                 </div>
               </div>
               <div className="message">
                 <div className="text-container">
                   <span className="span-label">Name:</span>
-                  {name}
+                  {edit ? (
+                    inputName
+                  ) : (
+                    <input
+                      type="text"
+                      value={inputName}
+                      onChange={(event) => {
+                        setInputName(event.target.value);
+                      }}
+                    />
+                  )}
                 </div>
               </div>
               <div className="message">
                 <div className="text-container">
                   <span className="span-label">Email:</span>
-                  {email}
+                  {edit ? (
+                    inputEmail
+                  ) : (
+                    <input
+                      type="text"
+                      value={inputEmail}
+                      onChange={(event) => {
+                        setInputEmail(event.target.value);
+                      }}
+                    />
+                  )}
                 </div>
               </div>
               <div className="btn-container">
-                <button type="button" className="edit-btn">
-                  Edit
-                </button>
+                {edit ? (
+                  <button
+                    type="button"
+                    className="edit-btn"
+                    onClick={(event) => handleEdit(event)}
+                  >
+                    Edit
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="edit-btn"
+                    onClick={(event) => handleSave(event)}
+                  >
+                    save
+                  </button>
+                )}
+
                 <button
                   type="button"
                   className="delete-btn"
